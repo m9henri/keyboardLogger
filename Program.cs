a019fd1abeb166097160023dc17e7e9ff56d697b
m9henri <missolhenri@gmail.com>
2026-03-15 19:00:19 +0100

    Initial commit (sigma)

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace keyboardLogger;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filename = "input.log";
+
+        if (!File.Exists(filename)) // failsafe
+        {
+            File.Create(filename).Close();
+        }
+
+        while(true)
+        {
+            string key = Console.ReadKey(true).Key.ToString();
+            File.AppendAllText(filename, key);
+        }
+    }
+}
